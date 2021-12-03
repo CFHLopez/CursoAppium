0f762281e9448ae4e4087926886c0aeffecb4ad9
CFHLopez <christian.lopezc@utem.cl>
2021-12-03 18:17:42 -0300

    se separa el test y se le da prioridades, ademas se cambia los before y after method por suite

--- src/main/java/utils/MetodosGenericos.java
+++ src/main/java/utils/MetodosGenericos.java
@@ -4,10 +4,16 @@ import conexion.DriverContext;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import io.qameta.allure.model.Status;
+import jdk.net.SocketFlow;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+
+import static reports.Report.addStep;
 
 public class MetodosGenericos {
     private AppiumDriver driver;
@@ -16,28 +22,57 @@ public class MetodosGenericos {
         PageFactory.initElements(new AppiumFieldDecorator(driver),this);
     }
     /**
+     * Variables
+     */
+    int segundos = 10;
+
+    /**
      * Metodos
      */
-    public void esperaIxplicita(){
+    private void esperaIxplicita(){
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         System.out.println("Espera Implicita");
     }
+    private boolean esperarObjeto(MobileElement elemento){
+        try {
+            System.out.println("Esperando elemento: "+elemento+", "+segundos+" segnudos, hasta que aparezca");
+            WebDriverWait wait = new WebDriverWait(DriverContext.getDriver(),segundos);
+            wait.until(ExpectedConditions.visibilityOf(elemento));
+            System.out.println("Se encontro el elemento ("+elemento+"), se retorna true");
+            return true;
+
+        }catch (Exception e){
+            System.out.println("No se encontró el elemento ("+elemento+"), se retorna false");
+            return false;
+        }
+    }
+
     public String inspeccionarElemento(String atributo, MobileElement elemento){
         System.out.println("displayed= "+ elemento.getAttribute(atributo));
         return elemento.getAttribute(atributo);
     }
+
     public void darClick(MobileElement elemento){
-        elemento.click();
+        if(esperarObjeto(elemento)){
+            elemento.click();
+            esperaIxplicita();
+            addStep("Click en elemento: "+elemento, false, Status.PASSED,false);
+        }
+        else{
+            addStep("Click en elemento: "+elemento,false, Status.FAILED,false);
+        }
     }
+
     public String retornarTexto(MobileElement elemento){
         return elemento.getText();
     }
+
     public void seleccionarElemento(List<MobileElement> lista, String palabra){
         // Recorremos la lista hasta encontrar la palabra requerida
         for (MobileElement elemeto: lista){
             if (elemeto.getText().contains(palabra)){
                 // Hacemos click en el elemento buscado
-                elemeto.click();
+                darClick(elemeto);
                 // Al encontrar el elemento no necesitamos seguir buscando
                 break;
             }
--- src/test/java/testSuite/TestCase01_CambiarIdiomaEspanol.java
+++ src/test/java/testSuite/TestCase01_CambiarIdiomaEspanol.java
@@ -2,12 +2,11 @@ package testSuite;
 
 import conexion.DriverContext;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 import pages.HomePage;
 
 import static conexion.DriverContext.setUp;
+import static reports.Report.finalAssert;
 
 public class TestCase01_CambiarIdiomaEspanol {
     private String nombreDispositivo = "emulator-5554";
@@ -18,7 +17,7 @@ public class TestCase01_CambiarIdiomaEspanol {
 
     protected HomePage homePage = null;
 
-    @BeforeMethod
+    @BeforeSuite
     public void iniciarSeccion(){
         //setUp("ZY327WFR7S","Android","C:\\Users\\chris\\Downloads\\Instagram.apk","ZY327WFR7S",false);
         setUp(nombreDispositivo,
@@ -29,18 +28,31 @@ public class TestCase01_CambiarIdiomaEspanol {
         );
     }
 
-    @AfterMethod
+    @AfterSuite
     public void cerrarSeccion(){
         DriverContext.quitDriver();
     }
 
-    @Test
-    public void validarCambioDeIdioma(){
+    @Test (priority = 1,description = "Validar visualización de Logo")
+    public void validarVisualizacionLogo(){
         homePage = new HomePage();
         Assert.assertEquals(homePage.visualizarLogo(),"true");
+        finalAssert();
+    }
+
+    @Test (priority = 2, description = "Validar Dar Click para el cambio de idioma")
+    public void validarDarClickIdioma(){
+        homePage = new HomePage();
         homePage.darClickIdioma();
+        finalAssert();
+    }
+
+    @Test (priority = 3, description = "Validar el cambio de idioma")
+    public void validarCambioIdioma(){
+        homePage = new HomePage();
         homePage.seleccionarIdioma("Español");
         Assert.assertEquals(homePage.retornarTextoSingUp(),"Crear cuenta nueva");
         Assert.assertEquals(homePage.retornarTextoLogin(),"Iniciar sesión");
+        finalAssert();
     }
 }
